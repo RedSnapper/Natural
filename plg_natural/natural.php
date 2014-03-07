a7f4672fd1246dec7edb794393ac18b48d061957
MrBenGriffin <ben@redsnapper.net>
2014-03-07 11:03:49 +0000

    Plugin filenames weren't working for summarising the plug-in.

--- plg_natural/natural.php
+++ plg_natural/natural.php
@@ -5,7 +5,7 @@ class PlgSystemNatural extends JPlugin {
 	public function onAfterInitialise() {
 		JLoader::register('JDocumentHTML', JPATH_LIBRARIES . '/natural/documenthtml.php', true);
 
-// JHtml is ONLY a monkey-patch for J3.21. See tracker# 32989
+// JHtml is ONLY a monkey-patch for J3.21-3. See tracker# 32989
 		JLoader::register('JHtml', JPATH_LIBRARIES . '/natural/html.php', true);
 
 // Helper registrations for JLoader
