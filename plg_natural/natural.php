41ce1de9ef58e3b394f6f7eaad7b98b9fa19e847
Ben Griffin <ben@redsnapper.net>
2014-04-23 10:36:58 +0100

    Updated date signatures and copyright notices.

--- plg_natural/natural.php
+++ plg_natural/natural.php
@@ -4,12 +4,14 @@ defined( '_JEXEC' ) or die;
 class PlgSystemNatural extends JPlugin {
 	public function onAfterInitialise() {
 
+// JControllerLegacy is ONLY a monkey-patch. See #3462 / Tracker #33623
 		JLoader::register('JControllerLegacy', JPATH_LIBRARIES . '/natural/legacy.php', true);
 
-		JLoader::register('JDocumentHTML', JPATH_LIBRARIES . '/natural/documenthtml.php', true);
-
-// JHtml is ONLY a monkey-patch for J3.21-3. See tracker# 32989
+// JHtml is ONLY a monkey-patch for J3.21-3. See #3224 / Tracker# 32989
 		JLoader::register('JHtml', JPATH_LIBRARIES . '/natural/html.php', true);
+
+// Add Natural View / XPath support
+		JLoader::register('JDocumentHTML', JPATH_LIBRARIES . '/natural/documenthtml.php', true);
 
 // Helper registrations for JLoader
 		JLoader::registerPrefix('N', JPATH_LIBRARIES . '/natural' );
