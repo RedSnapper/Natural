c49c9960d946411c21fc5b8feba9efeba82e78f4
Ben Griffin <ben@redsnapper.net>
2014-05-14 13:42:26 +0100

    debugging layouts..

--- com_composite/site/controller.php
+++ com_composite/site/controller.php
@@ -12,11 +12,62 @@ class CompositeController extends JControllerLegacy
 
 	public function display($cachable = false, $urlparams = false)
 	{
-		$view   = $this->input->get('view', 'contacts');
-		$layout = $this->input->get('layout', 'default', 'string');
-		$id     = $this->input->getInt('id');
-		parent::display();
+//exactly the same as parent::display..
+		$document = JFactory::getDocument();
+		$viewType = $document->getType();
+		$viewName = $this->input->get('view', $this->default_view);
+		$viewLayout = $this->input->get('layout', 'default', 'string');
+
+		$view = $this->getView($viewName, $viewType, '', array('base_path' => $this->basePath, 'layout' => $viewLayout));
+
+		// Get/Create the model
+		if ($model = $this->getModel($viewName))
+		{
+			// Push the model into the view (as default)
+			$view->setModel($model, true);
+		}
+
+		$view->document = $document;
+
+		$conf = JFactory::getConfig();
+
+		// Display the view
+		if ($cachable && $viewType != 'feed' && $conf->get('caching') >= 1)
+		{
+			$option = $this->input->get('option');
+			$cache = JFactory::getCache($option, 'view');
+
+			if (is_array($urlparams))
+			{
+				$app = JFactory::getApplication();
+
+				if (!empty($app->registeredurlparams))
+				{
+					$registeredurlparams = $app->registeredurlparams;
+				}
+				else
+				{
+					$registeredurlparams = new stdClass;
+				}
+
+				foreach ($urlparams as $key => $value)
+				{
+					// Add your safe url parameters with variable type as value {@see JFilterInput::clean()}.
+					$registeredurlparams->$key = $value;
+				}
+
+				$app->registeredurlparams = $registeredurlparams;
+			}
+
+			$cache->get($view, 'display');
+		}
+		else
+		{
+			$view->display();
+		}
+
 		return $this;
+
 	}
 
 
